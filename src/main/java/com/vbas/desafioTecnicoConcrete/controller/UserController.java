8e26ba8dbf7e44f22e16062c6d43b6ac7a8454d6
vbsilva <vbas@cin.ufpe.br>
2020-10-25 11:26:17 -0300

    basic security config

--- src/main/java/com/vbas/desafioTecnicoConcrete/controller/UserController.java
+++ src/main/java/com/vbas/desafioTecnicoConcrete/controller/UserController.java
@@ -5,10 +5,7 @@ import com.vbas.desafioTecnicoConcrete.model.User;
 import com.vbas.desafioTecnicoConcrete.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -19,6 +16,11 @@ public class UserController {
     @PostMapping("/createUser")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "ae";
     }
 
 
